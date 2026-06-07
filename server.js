fd12eb539e52f13e8a80e2be7783d482e858fd4a
wer1x <wer1xtr@gmail.com>
2026-06-08 00:42:50 +0300

    Detaylı cihaz model tespiti - iPhone/iPad/Android

--- server.js
+++ server.js
@@ -48,9 +48,57 @@ function parseUA(ua) {
   else if (/TikTok/i.test(ua)) app = 'TikTok';
   else if (/Reddit/i.test(ua)) app = 'Reddit';
   else if (/okhttp/i.test(ua)) app = 'Android Uygulama';
-  if (/iPhone/i.test(ua)) { device = 'iPhone'; os = 'iOS'; }
-  else if (/iPad/i.test(ua)) { device = 'iPad'; os = 'iOS'; }
-  else if (/Android/i.test(ua)) device = /Mobile/i.test(ua) ? 'Android Telefon' : 'Android Tablet';
+  if (/iPhone/i.test(ua)) {
+    os = 'iOS';
+    const iphones = {
+      'iPhone1,1':'iPhone','iPhone1,2':'iPhone 3G','iPhone2,1':'iPhone 3GS','iPhone3,1':'iPhone 4','iPhone3,3':'iPhone 4','iPhone4,1':'iPhone 4S',
+      'iPhone5,1':'iPhone 5','iPhone5,2':'iPhone 5','iPhone5,3':'iPhone 5c','iPhone5,4':'iPhone 5c','iPhone6,1':'iPhone 5s','iPhone6,2':'iPhone 5s',
+      'iPhone7,1':'iPhone 6 Plus','iPhone7,2':'iPhone 6','iPhone8,1':'iPhone 6s','iPhone8,2':'iPhone 6s Plus','iPhone8,4':'iPhone SE',
+      'iPhone9,1':'iPhone 7','iPhone9,3':'iPhone 7','iPhone9,2':'iPhone 7 Plus','iPhone9,4':'iPhone 7 Plus',
+      'iPhone10,1':'iPhone 8','iPhone10,4':'iPhone 8','iPhone10,2':'iPhone 8 Plus','iPhone10,5':'iPhone 8 Plus','iPhone10,3':'iPhone X','iPhone10,6':'iPhone X',
+      'iPhone11,2':'iPhone XS','iPhone11,4':'iPhone XS Max','iPhone11,6':'iPhone XS Max','iPhone11,8':'iPhone XR',
+      'iPhone12,1':'iPhone 11','iPhone12,3':'iPhone 11 Pro','iPhone12,5':'iPhone 11 Pro Max','iPhone12,8':'iPhone SE 2',
+      'iPhone13,1':'iPhone 12 mini','iPhone13,2':'iPhone 12','iPhone13,3':'iPhone 12 Pro','iPhone13,4':'iPhone 12 Pro Max',
+      'iPhone14,2':'iPhone 13 Pro','iPhone14,3':'iPhone 13 Pro Max','iPhone14,4':'iPhone 13 mini','iPhone14,5':'iPhone 13','iPhone14,6':'iPhone SE 3',
+      'iPhone14,7':'iPhone 14','iPhone14,8':'iPhone 14 Plus',
+      'iPhone15,1':'iPhone 14 Pro (n84)','iPhone15,2':'iPhone 14 Pro Max','iPhone15,3':'iPhone 15','iPhone15,4':'iPhone 15 Plus','iPhone15,5':'iPhone 15 Pro','iPhone15,6':'iPhone 15 Pro Max',
+      'iPhone16,1':'iPhone 16 Pro','iPhone16,2':'iPhone 16 Pro Max','iPhone16,3':'iPhone 16','iPhone16,4':'iPhone 16 Plus',
+      'iPhone16,5':'iPhone 16e','iPhone17,1':'iPhone 17','iPhone17,2':'iPhone 17 Plus','iPhone17,3':'iPhone 17 Pro','iPhone17,4':'iPhone 17 Pro Max'
+    };
+    let found = '';
+    for (const [id, name] of Object.entries(iphones)) {
+      if (ua.includes(id)) { found = name; break; }
+    }
+    device = found || 'iPhone';
+  } else if (/iPad/i.test(ua)) {
+    os = 'iOS';
+    const ipads = {
+      'iPad1,1':'iPad','iPad2,1':'iPad 2','iPad2,2':'iPad 2','iPad2,3':'iPad 2','iPad2,4':'iPad 2','iPad2,5':'iPad mini','iPad2,6':'iPad mini','iPad2,7':'iPad mini',
+      'iPad3,1':'iPad 3','iPad3,2':'iPad 3','iPad3,3':'iPad 3','iPad3,4':'iPad 4','iPad3,5':'iPad 4','iPad3,6':'iPad 4',
+      'iPad4,1':'iPad Air','iPad4,2':'iPad Air','iPad4,3':'iPad Air','iPad4,4':'iPad mini 2','iPad4,5':'iPad mini 2','iPad4,6':'iPad mini 2','iPad4,7':'iPad mini 3','iPad4,8':'iPad mini 3','iPad4,9':'iPad mini 3',
+      'iPad5,1':'iPad mini 4','iPad5,2':'iPad mini 4','iPad5,3':'iPad Air 2','iPad5,4':'iPad Air 2',
+      'iPad6,3':'iPad Pro 9.7','iPad6,4':'iPad Pro 9.7','iPad6,7':'iPad Pro 12.9','iPad6,8':'iPad Pro 12.9','iPad6,11':'iPad 5','iPad6,12':'iPad 5',
+      'iPad7,1':'iPad Pro 12.9 2G','iPad7,2':'iPad Pro 12.9 2G','iPad7,3':'iPad Pro 10.5','iPad7,4':'iPad Pro 10.5','iPad7,5':'iPad 6','iPad7,6':'iPad 6','iPad7,11':'iPad 7','iPad7,12':'iPad 7',
+      'iPad8,1':'iPad Pro 11','iPad8,2':'iPad Pro 11','iPad8,3':'iPad Pro 11','iPad8,4':'iPad Pro 11','iPad8,5':'iPad Pro 12.9 3G','iPad8,6':'iPad Pro 12.9 3G','iPad8,7':'iPad Pro 12.9 3G','iPad8,8':'iPad Pro 12.9 3G',
+      'iPad8,9':'iPad Pro 11 2G','iPad8,10':'iPad Pro 11 2G','iPad8,11':'iPad Pro 12.9 4G','iPad8,12':'iPad Pro 12.9 4G',
+      'iPad11,1':'iPad mini 5','iPad11,2':'iPad mini 5','iPad11,3':'iPad Air 3','iPad11,4':'iPad Air 3','iPad11,6':'iPad 8','iPad11,7':'iPad 8',
+      'iPad12,1':'iPad 9','iPad12,2':'iPad 9',
+      'iPad13,1':'iPad Air 4','iPad13,2':'iPad Air 4','iPad13,4':'iPad Pro 11 3G','iPad13,5':'iPad Pro 11 3G','iPad13,6':'iPad Pro 11 3G','iPad13,7':'iPad Pro 11 3G',
+      'iPad13,8':'iPad Pro 12.9 5G','iPad13,9':'iPad Pro 12.9 5G','iPad13,10':'iPad Pro 12.9 5G','iPad13,11':'iPad Pro 12.9 5G',
+      'iPad13,16':'iPad Air 5','iPad13,17':'iPad Air 5','iPad13,18':'iPad 10','iPad13,19':'iPad 10',
+      'iPad14,1':'iPad mini 6','iPad14,2':'iPad mini 6',
+      'iPad14,3':'iPad Pro 11 4G','iPad14,4':'iPad Pro 11 4G','iPad14,5':'iPad Pro 12.9 6G','iPad14,6':'iPad Pro 12.9 6G',
+      'iPad14,8':'iPad Air 6 (11)','iPad14,9':'iPad Air 6 (11)','iPad14,10':'iPad Air 6 (13)','iPad14,11':'iPad Air 6 (13)',
+      'iPad15,3':'iPad Pro 11 5G (M4)','iPad15,4':'iPad Pro 11 5G (M4)','iPad15,5':'iPad Pro 13 (M4)','iPad15,6':'iPad Pro 13 (M4)','iPad15,7':'iPad Air 7','iPad15,8':'iPad Air 7',
+      'iPad16,1':'iPad 11','iPad16,2':'iPad 11','iPad16,3':'iPad mini 7','iPad16,4':'iPad mini 7','iPad16,5':'iPad Pro 11 6G','iPad16,6':'iPad Pro 11 6G','iPad16,7':'iPad Pro 13 2G','iPad16,8':'iPad Pro 13 2G'
+    };
+    let found = '';
+    for (const [id, name] of Object.entries(ipads)) { if (ua.includes(id)) { found = name; break; } }
+    device = found || 'iPad';
+  } else if (/Android/i.test(ua)) {
+    let m = ua.match(/\(([^;]+);\s*([^;)]+)/);
+    device = m ? m[2].trim() : (/Mobile/i.test(ua) ? 'Android Telefon' : 'Android Tablet');
+  }
   if (/Windows NT 10/i.test(ua)) os = 'Windows 10';
   else if (/Windows NT 11/i.test(ua)) os = 'Windows 11';
   else if (/Windows NT 6\.3/i.test(ua)) os = 'Windows 8.1';
